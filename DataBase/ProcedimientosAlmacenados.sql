e7ecedced4586a503a596e2c1a384a51d1b1bbe9
LuFerLux <luferlira@gmail.com>
2016-07-08 16:48:17 -0500

    P.A.

--- DataBase/ProcedimientosAlmacenados.sql
+++ DataBase/ProcedimientosAlmacenados.sql
@@ -1,24 +1,48 @@
 Use `BDLA12` ;
 
 DELIMITER //
-CREATE PROCEDURE AutenticarUsuario(in _email VARCHAR(50), in _password VARCHAR(60))     /* Parámetro de entrada */
+CREATE PROCEDURE AutenticarUsuario(in _email VARCHAR(50), in _password VARCHAR(60))
 BEGIN
-  SELECT * FROM Usuario WHERE email=_email && password =_password;
+  SELECT * FROM Usuario WHERE email=_email AND password =_password AND NOT estado!='0001' ;
 END
 //
 
 DELIMITER //
 CREATE PROCEDURE CrearReserva(in idCliente INT, in fechaReserva DATE)
 BEGIN
-  DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
-  BEGIN
-    ROLLBACK;
-  END;
+  /*Estado por defecto EN_PROCESO*/
+  DECLARE default_estado CHAR(4);
+  SET default_estado = '0002';
+
   START TRANSACTION;
-    INSERT INTO Reserva VALUES(DEFAULT, NOW(), fechaReserva, '0002', O.O, idCliente);
+    INSERT INTO Reserva VALUES(DEFAULT, NOW(), fechaReserva, default_estado, O.O, idCliente);
   COMMIT;
+END;
+//
+
+DELIMITER //
+CREATE PROCEDURE AgregarDetalleReserva(in horaInicio TIME, in horaFin TIME, in _idReserva INT, in idCancha INT)
+BEGIN
+  DECLARE subTotal DECIMAL(10,2);
+  DECLARE idDetalleReserva INT;
+  SELECT FROM Reserva WHERE
+  SET idDetalleReserva = (SELECT idDetalleReserva FROM DetalleReserva
+    WHERE horaInicio=_horahoraInicio AND horaFin=_horahoraFin AND idReserva=_idReserva)
+  IF idDetalleReserva!=NULL
+  BEGIN
+    START TRANSACTION;
+      INSERT INTO DetalleReserva VALUES(DEFAULT, horaInicio, horaFin, subTotal, idReserva, idCancha);
+    COMMIT;
+  END;
+  ELSE
+  BEGIN
+    SELECT 'El Detalle De Reserva Ya Existe' AS 'msg_error';
+  END;
+
 END
 //
+
+
 
 DELIMITER $$
 CREATE PROCEDURE paAsignarOperador (nomSede varchar(50), dniOpe char(8), fechaIni time)
@@ -35,3 +59,4 @@ BEGIN
     INSERT INTO dirige (inicio, Dirige_idSede, Dirige_idOperador) VALUES (fechaIni, idSe, idOpe);
 COMMIT;
 END;
+$$
