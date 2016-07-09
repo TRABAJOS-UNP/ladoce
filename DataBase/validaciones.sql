d731233b929cead00e0a70b89b8c2ffb32c8ae9f
EmilioPantaleon <emilio.pantaleon90@gmail.com>
2016-07-09 14:10:42 -0500

    Update validaciones.sql

--- DataBase/validaciones.sql
+++ DataBase/validaciones.sql
@@ -1,6 +1,6 @@
 DROP TRIGGER IF EXISTS check_ins_cancha;
 DELIMITER $$
-CREATE TRIGGER check_ins_cancha BEFORE INSERT ON cancha FOR EACH ROW 
+CREATE TRIGGER check_ins_cancha BEFORE INSERT ON Cancha FOR EACH ROW 
 BEGIN
 	if numero < 0  THEN
     		SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error numero mayor a 0';
@@ -12,7 +12,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_cancha;
 DELIMITER $$
-CREATE TRIGGER check_upd_cancha BEFORE UPDATE ON cancha FOR EACH ROW 
+CREATE TRIGGER check_upd_cancha BEFORE UPDATE ON Cancha FOR EACH ROW 
 BEGIN
 	if numero < 0  THEN
     		SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error numero mayor a 0';
@@ -24,7 +24,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_cliente;
 DELIMITER $$
-CREATE TRIGGER check_ins_cliente BEFORE INSERT ON cliente FOR EACH ROW 
+CREATE TRIGGER check_ins_cliente BEFORE INSERT ON Cliente FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -33,7 +33,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_cliente;
 DELIMITER $$
-CREATE TRIGGER check_upd_cliente BEFORE UPDATE ON cliente FOR EACH ROW 
+CREATE TRIGGER check_upd_cliente BEFORE UPDATE ON Cliente FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -42,7 +42,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_departamento;
 DELIMITER $$
-CREATE TRIGGER check_ins_departamento BEFORE INSERT ON departamento FOR EACH ROW 
+CREATE TRIGGER check_ins_departamento BEFORE INSERT ON Departamento FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -51,7 +51,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_departamento;
 DELIMITER $$
-CREATE TRIGGER check_upd_departamento BEFORE UPDATE ON departamento FOR EACH ROW 
+CREATE TRIGGER check_upd_departamento BEFORE UPDATE ON Departamento FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -60,7 +60,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_detallereserva;
 DELIMITER $$
-CREATE TRIGGER check_ins_detallereserva BEFORE INSERT ON detallereserva FOR EACH ROW 
+CREATE TRIGGER check_ins_detallereserva BEFORE INSERT ON DetalleReserva FOR EACH ROW 
 BEGIN
     	if new.subTotal < 0.0 THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error sub Total mayor a 0;
@@ -69,7 +69,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_detallereserva;
 DELIMITER $$
-CREATE TRIGGER check_upd_detallereserva BEFORE UPDATE ON detallereserva FOR EACH ROW 
+CREATE TRIGGER check_upd_detallereserva BEFORE UPDATE ON DetalleReserva FOR EACH ROW 
 BEGIN
     	if new.subTotal < 0.0 THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error sub Total mayor a 0;
@@ -78,7 +78,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_distrito;
 DELIMITER $$
-CREATE TRIGGER check_ins_distrito BEFORE INSERT ON distrito FOR EACH ROW 
+CREATE TRIGGER check_ins_distrito BEFORE INSERT ON Distrito FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -87,7 +87,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_distrito;
 DELIMITER $$
-CREATE TRIGGER check_upd_distrito BEFORE UPDATE ON distrito FOR EACH ROW 
+CREATE TRIGGER check_upd_distrito BEFORE UPDATE ON Distrito FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -96,7 +96,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_gerente;
 DELIMITER $$
-CREATE TRIGGER check_ins_gerente BEFORE INSERT ON gerente FOR EACH ROW 
+CREATE TRIGGER check_ins_gerente BEFORE INSERT ON Gerente FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -105,7 +105,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_gerente;
 DELIMITER $$
-CREATE TRIGGER check_upd_gerente BEFORE UPDATE ON gerente FOR EACH ROW 
+CREATE TRIGGER check_upd_gerente BEFORE UPDATE ON Gerente FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -114,7 +114,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_historialcancha;
 DELIMITER $$
-CREATE TRIGGER check_ins_historialcancha BEFORE INSERT ON historialcancha FOR EACH ROW 
+CREATE TRIGGER check_ins_historialcancha BEFORE INSERT ON HistorialCancha FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -123,7 +123,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_historialcancha;
 DELIMITER $$
-CREATE TRIGGER check_upd_historialcancha BEFORE UPDATE ON historialcancha FOR EACH ROW 
+CREATE TRIGGER check_upd_historialcancha BEFORE UPDATE ON HistorialCancha FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -132,7 +132,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_historialsede;
 DELIMITER $$
-CREATE TRIGGER check_ins_historialsede BEFORE INSERT ON historialsede FOR EACH ROW 
+CREATE TRIGGER check_ins_historialsede BEFORE INSERT ON HistorialSede FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -141,7 +141,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_historialsede;
 DELIMITER $$
-CREATE TRIGGER check_upd_historialsede BEFORE UPDATE ON historialsede FOR EACH ROW 
+CREATE TRIGGER check_upd_historialsede BEFORE UPDATE ON HistorialSede FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -150,7 +150,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_operador;
 DELIMITER $$
-CREATE TRIGGER check_ins_operador BEFORE INSERT ON operador FOR EACH ROW 
+CREATE TRIGGER check_ins_operador BEFORE INSERT ON Operador FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -159,7 +159,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_operador;
 DELIMITER $$
-CREATE TRIGGER check_upd_operador BEFORE UPDATE ON operador FOR EACH ROW 
+CREATE TRIGGER check_upd_operador BEFORE UPDATE ON Operador FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -168,7 +168,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_parametro;
 DELIMITER $$
-CREATE TRIGGER check_ins_parametro BEFORE INSERT ON parametro FOR EACH ROW 
+CREATE TRIGGER check_ins_parametro BEFORE INSERT ON Parametro FOR EACH ROW 
 BEGIN
     	if new.sep1 not rlike '[0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error sep1 - solo digitos';
@@ -180,7 +180,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_parametro;
 DELIMITER $$
-CREATE TRIGGER check_upd_parametro BEFORE UPDATE ON parametro FOR EACH ROW 
+CREATE TRIGGER check_upd_parametro BEFORE UPDATE ON Parametro FOR EACH ROW 
 BEGIN
     	if new.sep1 not rlike '[0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error sep1 - solo digitos';
@@ -192,7 +192,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_preciohora;
 DELIMITER $$
-CREATE TRIGGER check_ins_preciohora BEFORE INSERT ON preciohora FOR EACH ROW 
+CREATE TRIGGER check_ins_preciohora BEFORE INSERT ON PrecioHora FOR EACH ROW 
 BEGIN
 	if new.diaSemana not IN ('L','M','X','J','V','S','D') THEN
     		SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error dia semana';
@@ -213,7 +213,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_preciohora;
 DELIMITER $$
-CREATE TRIGGER check_upd_preciohora BEFORE UPDATE ON preciohora FOR EACH ROW 
+CREATE TRIGGER check_upd_preciohora BEFORE UPDATE ON PrecioHora FOR EACH ROW 
 BEGIN
 	if new.diaSemana not IN ('L','M','X','J','V','S','D') THEN
     		SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error dia semana';
@@ -234,7 +234,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_provincia;
 DELIMITER $$
-CREATE TRIGGER check_ins_provincia BEFORE INSERT ON provincia FOR EACH ROW 
+CREATE TRIGGER check_ins_provincia BEFORE INSERT ON Provincia FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -243,7 +243,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_provincia;
 DELIMITER $$
-CREATE TRIGGER check_upd_provincia BEFORE UPDATE ON provincia FOR EACH ROW 
+CREATE TRIGGER check_upd_provincia BEFORE UPDATE ON Provincia FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -252,7 +252,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_reserva;
 DELIMITER $$
-CREATE TRIGGER check_ins_reserva BEFORE INSERT ON reserva FOR EACH ROW 
+CREATE TRIGGER check_ins_reserva BEFORE INSERT ON Reserva FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -264,7 +264,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_reserva;
 DELIMITER $$
-CREATE TRIGGER check_upd_reserva BEFORE UPDATE ON reserva FOR EACH ROW 
+CREATE TRIGGER check_upd_reserva BEFORE UPDATE ON Reserva FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -276,7 +276,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_sede;
 DELIMITER $$
-CREATE TRIGGER check_ins_sede BEFORE INSERT ON sede FOR EACH ROW 
+CREATE TRIGGER check_ins_sede BEFORE INSERT ON Sede FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -285,7 +285,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_sede;
 DELIMITER $$
-CREATE TRIGGER check_upd_sede BEFORE UPDATE ON sede FOR EACH ROW 
+CREATE TRIGGER check_upd_sede BEFORE UPDATE ON Sede FOR EACH ROW 
 BEGIN
     	if new.estado not rlike '[0-9][0-9][0-9][0-9]' THEN
 	   	SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error estado - solo digitos';
@@ -294,7 +294,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_ins_usuario;
 DELIMITER $$
-CREATE TRIGGER check_ins_usuario BEFORE INSERT ON usuario FOR EACH ROW 
+CREATE TRIGGER check_ins_usuario BEFORE INSERT ON Usuario FOR EACH ROW 
 BEGIN
 	if CHAR_length(new.password) < 8 THEN
     		SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error password - mayor de 7 caracteres';
@@ -315,7 +315,7 @@ END;
 
 DROP TRIGGER IF EXISTS check_upd_usuario;
 DELIMITER $$
-CREATE TRIGGER check_upd_usuario BEFORE UPDATE ON usuario FOR EACH ROW 
+CREATE TRIGGER check_upd_usuario BEFORE UPDATE ON Usuario FOR EACH ROW 
 BEGIN
 	if CHAR_length(new.password) < 8 THEN
     		SIGNAL SQLSTATE '45000' set MESSAGE_TEXT = 'Error password - mayor de 7 caracteres';
